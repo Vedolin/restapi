f752e9ea8348cff35427fad125e2d7d647649236
Vedolin <vedolin@yahoo.com>
2019-11-07 13:23:30 -0300

    add src/test/postman collection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1,'2019-10-31', 'Corsa', 2000312);
-insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (2,'2018-01-10', 'Corsa', 1512530);
-insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (3,'1975-04-15', 'Chevette', 3500000);
-insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (4,'1986-07-02', 'Fusca', 2598698);
-insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (5,'2019-11-07', 'Corolla', 6254300);
+insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1001,'2019-10-31', 'Corsa', 2000312);
+insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1002,'2018-01-10', 'Corsa', 1512530);
+insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1003,'1975-04-15', 'Chevette', 3500000);
+insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1004,'1986-07-02', 'Fusca', 2598698);
+insert into carro (ID, DATA_COMPRA, MODELO, VALOR) values (1005,'2019-11-07', 'Corolla', 6254300);
